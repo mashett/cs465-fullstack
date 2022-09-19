ab587245ca1b9c594be9f7903c54b86d497d13d5
darwin <paul.mashett@gmail.com>
2022-09-18 22:38:14 -0400

    Convert travel template from static data to dynamic JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,12 @@
+//const fs = require('fs');
+var fs = require('fs'); 
+
+//const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways' });
+    res.render('travel', { title: 'Travlr Getaways', trips });
 };
 
 module.exports = {
